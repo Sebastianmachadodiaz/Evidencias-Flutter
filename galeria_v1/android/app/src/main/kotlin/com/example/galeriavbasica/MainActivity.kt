81d2ad997c980c7f9bc8af88895fc857f53dfc07
Sebastian Machado <sebastianmachado099@gmail.com>
2022-12-21 01:01:03 -0500

    Evidencia de la primera version de la galeria con 4 imagenes

---
+++ galeria_v1/android/app/src/main/kotlin/com/example/galeriavbasica/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.galeriavbasica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ galeria_v1/lib/my_app.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Galeria con conatiner',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Galeria construida con container'),
+        ),
+        body: SingleChildScrollView(
+          child: Center(child: _buildImageColumn()),
+        ),
+      ),
+    );
+  }
+  Widget _buildImageColumn(){
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black26,
+      ),
+      child: ClipRect(
+        child: Align(
+          alignment: Alignment.center,
+          widthFactor: 1,
+          heightFactor: 1,
+          child: Column(
+            children: [
+              _buildImageRow(1),
+              _buildImageRow(3),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _buildDecoratedImage(int imageIndex)=>Expanded(
+      child:Container(
+        width: 150,
+        decoration: BoxDecoration(
+          border: Border.all(width: 10, color: Colors.black38),
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+        ),
+        margin: const EdgeInsets.all(4),
+        child: Image.asset('images/pic$imageIndex.jpg'),
+      ),
+  );
+
+  Widget _buildImageRow(int imageIndex)=> Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
+}
